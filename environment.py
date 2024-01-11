da1575acccd79c591c3eab1ead31ad52ba78dd54
juttevijverberg <144141087+juttevijverberg@users.noreply.github.com>
2024-01-11 18:49:35 +0100

    pushhh

--- environment.py
+++ environment.py
@@ -1,9 +1,9 @@
 import gymnasium as gym
 from gym import spaces
 import numpy as np
-# import time
-# import matplotlib.pyplot as plt
-# import time
+import time
+import matplotlib.pyplot as plt
+import time
 import random
 from numpy.random import default_rng
 
@@ -14,64 +14,56 @@ class Environment(gym.Env):
         self.data = data
         self.max_capacity = 50 #kWh
         self.efficiency = 0.9
-        self.rng = default_rng()
-        self.battery_valuation = 1000
-        
         self.power = 25 #kW
         self.morning_capacity = 20 #kWh
+        self.rng = default_rng()
+        self.battery_valuation = 20
         self.min_price = 0
         self.max_price = 10000
     
         #Define action space
-        # 0-24 sell
+        # 0-24 kWh sell
         # 25 nothing
-        # 26-50 buy
+        # 26-50 kWh buy
         self.action_space = spaces.Discrete(51)
         
         #Define observation space
         # battery, hour, price, availability
+        
         self.observation_space = spaces.Dict({
             'battery': spaces.Box(low=0, high=self.max_capacity, shape=(1,), dtype=np.float32),
             'hour': spaces.Discrete(24),
-            'price': spaces.Box(low=0, high=self.max_price, shape=(1,)),
+            'price': spaces.Box(low=self.min_price, high=self.max_price, dtype=np.float32),
             'availability': spaces.Discrete(2)  # 0: available, 1: unavailable
         })
-        self.state = dict()
-        self.reset()
-
-
+        
+        self.state = {
+            'battery': self.morning_capacity,
+            'hour': 0,
+            'price': data.loc[data['day'] == 0, 'price'].values,
+            'availability': random.randint(0,1)
+        }
+    
     def reset(self):
-        """
-        Reset the environment to the initial state
-        Returns the initial state
-        """
-        self.__index = 0
-        self.t = self.data.iloc[self.__index]
+        # Reset the environment to the initial state
+        self.state = {
+            'battery': self.morning_capacity,
+            'hour': 8,
+            'price': data.loc[data['day'] == 0, 'price'].values,
+            'availability': random.randint(0,1)
+        }
         
-
-        self.state['hour'] = self.t.Hour
-        self.state['battery'] = self.morning_capacity
-        self.state['price'] = self.data.loc[self.data['Date'] == 0]['Price'].values
-        self.state['availability'] = random.randint(0,1)
-        
-        return self.state
+        return self.state, 0
     
     def step(self, action):
-        # value_battery = self.state['battery']*self.battery_valuation
-        
         if action < 25: #sell
             reward = -(action-25)*self.efficiency*(self.battery_valuation - self.state['price'])
-        elif action == 25:
-            reward = 0
-        else: # buy
-            reward = (action-25)*self.efficiency*(self.battery_valuation - self.state['price'])
+        elif action > 25: #buy
+            reward = 2*(action-25)*self.efficiency*(self.battery_valuation - self.state['price'])
         
-        self.__index +=1 
-        self.t = self.data.iloc[self.__index]
-        self.state['hour'] = self.t.hour
-        
-        self.state['battery'] += action*self.efficiency
-        self.state['price'] = self.data.loc[self.data['Date'] == self.date, 'Price'].values,
-        self.state['availability'] = random.randint(0,1)
-        
+        self.state['battery'] += (action-25)*self.efficiency 
+        if self.state['hour'] == 24:
+            self
+            self.state['hour'] +=1
+
         return self.state, reward
